b1dd9c9ce80bd02306689f8904473fb04590bdf6
Malik Piara <malik@hey.com>
2024-12-14 16:27:29 +0000

    Added 192 icon.

--- app/manifest.ts
+++ app/manifest.ts
@@ -9,16 +9,19 @@ export default function manifest(): MetadataRoute.Manifest {
     display: 'standalone',
     background_color: '#ffffff',
     theme_color: '#ffffff',
+    orientation: 'portrait',
     icons: [
       {
-        src: '/icon-192x192.png',
+        src: '/icon192.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'maskable',
       },
       {
         src: '/icon512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   };
