93aa95af788ddd6371681614a6fd0fcd243a4d0a
Malik Piara <malik@hey.com>
2024-12-28 15:52:52 +0000

    making form download a pdf when submitted.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,18 @@
-import { SalesForm } from '@/components/form';
+'use client';
+import dynamic from 'next/dynamic';
+
+// Dynamically import the SalesForm component with client-side only rendering
+const SalesForm = dynamic(
+  () => import('@/components/form').then((mod) => mod.SalesForm),
+  {
+    ssr: false,
+    loading: () => (
+      <div className='p-8 m-14'>
+        <p>A carregar...</p>
+      </div>
+    ),
+  }
+);
 
 export default function Home() {
   return (
